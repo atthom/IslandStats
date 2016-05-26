b233ef24fb802c32ed879b0164a261edd917189f
Thomas Jalabert <thom.jalabert@gmail.com>
2016-05-27 01:26:07 +0200

    add rate for cost and use for each actions

--- globalstats.py
+++ globalstats.py
@@ -18,7 +18,7 @@ exploit = []
 transform = []
 
 listaction = ["fly", "heading", "echo", "scan", "stop", "land", "move_to", "scout", "glimpse", "explore", "exploit", "transform"]
-listweek = ["week01", "week02", "week04", "week05", "week06", "week07", "week10", "week11", "week12", "week46", "week47", "week48",  "week49", "week50", "week5",  "wee52",  "week53"]
+listweek = ["week46", "week47", "week48",  "week49", "week50", "week51",  "wee52",  "week53", "week01", "week02", "week04", "week05", "week06", "week07","week08" "week09", "week10", "week11", "week12"]
 listteam = ["qaa",  "qac",  "qae",  "qba",  "qbc",  "qbf",  "qcb",  "qce",  "qdb",  "qdd", "qab",  "qad",  "qaf",  "qbb",  "qbe",  "qca",  "qcc",  "qda",  "qdc",  "qde", "qdf"]
 
 alldata = doc.getElementsByTagName("data")
@@ -39,14 +39,8 @@ def dataaction(action):
 	if sum_long==0:
 		print "L'action %s n'a pas été utilisé" % (action)
 	else:
-		print "il y a eu %d %s, de coup moyen %f et de coup total %f" % (sum_long, action, sum_mean, sum_mean*sum_long)
+		print "il y a eu %d %s, de coup moyen %f et de coup total %f" % (sum_long, action, sum_mean, float(sum_mean)*float(sum_long))
 	nbaction =0;
-
-for act in listaction:
-	dataaction(act)
-#for action in listaction:
-#	alldataforaction = doc.getElementsByTagName(action)
-
 
 def tauxdereussiteglobal():
 	nbrun = len(alldata)
@@ -56,9 +50,9 @@ def tauxdereussiteglobal():
 
 	taux = float(nbstop) / float(nbrun)
 	
-	print "Il y a eu %d explorations avec %d réussie, donc un taux de réussite de %f " % (nbrun, nbstop, taux)
+	print "Il y a eu %d explorations avec %d réussie, donc un taux de réussite de %f%% " % (nbrun, nbstop, taux*100)
 
-tauxdereussiteglobal()
+
 
 def goodteamgoodweek(datateam, dataweek, team="", week=""):
 	if team!="" and datateam!=team:
@@ -80,7 +74,7 @@ def tauxdereussite(team="", week=""):
 			nbstop = nbstop + int(getelement(stp, "longeur"))
 			nbrun = nbrun +1
 	taux = float(nbstop) / float(nbrun)
-	print "Il y a eu %d explorations avec %d réussie, donc un taux de réussite de %f " % (nbrun, nbstop, taux)
+	print "Il y a eu %d explorations avec %d réussie, donc un taux de réussite de %f%% " % (nbrun, nbstop, taux*100)
 
 
 def tauxdereussitebyteam(team):
@@ -95,7 +89,7 @@ def tauxdereussitebyteam(team):
 						nbrun = nbrun +1
 	if nbrun!=0:
 		taux = float(nbstop) / float(nbrun)
-		print "Il y a eu %d explorations pour l'équipe %s avec %d réussie, donc un taux de réussite de %f " % (nbrun, team, nbstop, taux)
+		print "Il y a eu %d explorations pour l'équipe %s avec %d réussie, donc un taux de réussite de %f%% " % (nbrun, team, nbstop, taux*100)
 
 def tauxdereussitebyweek(week):
 	nbstop = 0
@@ -109,10 +103,9 @@ def tauxdereussitebyweek(week):
 						nbrun = nbrun +1
 	if nbrun!=0:
 		taux = float(nbstop) / float(nbrun)
-		print "Au cours de la semaine %s, Il y a eu %d explorations avec %d réussie, donc un taux de réussite de %f " % (week, nbrun, nbstop, taux)
+		print "Au cours de la semaine %s, Il y a eu %d explorations avec %d réussie, donc un taux de réussite de %f%% " % (week, nbrun, nbstop, taux*100)
 
-def dataaction(week):
-	alldata = doc.getElementsByTagName("data")
+def dataweek(week):
 	sum_mean= 0.0
 	sum_long= 0.0
 	for data in alldata:
@@ -128,13 +121,68 @@ def dataaction(week):
 		print "il y a eu %d %s, de coup moyen %f et de coup total %f" % (sum_long, action, sum_mean, sum_mean*sum_long)
 	nbaction =0;
 
+def globalaction():
+	sum_cost = 0.0
+	sum_long = 0.0
+	for action in listaction:
+		allaction = doc.getElementsByTagName(action)
+		for act in allaction:
+			current_long = int(getelement(act,"longeur"))
+			current_moy = float(getelement(act,"moyenne"))
+			if current_long!=0:
+				sum_cost = sum_cost + float(current_long)*current_moy
+				sum_long = sum_long + current_long
+
+	print "Il y a eu %d actions de coup total %f" % (sum_long, sum_cost)
+
+
+def getnbactionandtotalcostbyaction(action):
+	sum_cost = 0.0
+	sum_long = 0
+	allaction = doc.getElementsByTagName(action)
+	for act in allaction:
+		current_long = int(getelement(act,"longeur"))
+		current_moy = float(getelement(act,"moyenne"))
+		if current_long!=0:
+			sum_cost = sum_cost + float(current_long)*current_moy
+			sum_long = sum_long + current_long
+	return [sum_cost, sum_long]
+
+def globaltauxactions():
+	sum_cost = 0.0
+	sum_long = 0
+	listall = []
+	actionrate = []
+	for action in listaction:
+		listall.append(getnbactionandtotalcostbyaction(action))
+	for oneact in listall:
+		sum_cost = sum_cost + oneact[0]
+		sum_long = sum_long + oneact[1]
+	for action in listall:
+		rate_cost = float(action[0]) / float(sum_cost)
+		rate_long = float(action[1]) / float(sum_long)
+		actionrate.append([rate_cost, rate_long])
+	i =0
+	for rate in actionrate:
+		print "L'action %s a un taux d'utilisation de %f%%  et un taux de cout de %f%%" % (listaction[i], rate[1]*100, rate[0]*100)
+		i = i+1
+	print "\nIl y a eu au total %d actions pour un coup total de %d et un coup moyen de %f par actions" % (sum_long, sum_cost, float(sum_cost)/float(sum_long) )
+
 def tauxdereussitedetail():
-	print "\n****************\n"
 	for team in listteam:
 		tauxdereussitebyteam(team)
 	print "\n****************\n"
 	for week in listweek:
 		tauxdereussitebyweek(week)
 
+for act in listaction:
+	dataaction(act)
+
+print "\n****************\n"
+
+tauxdereussiteglobal()
+print "\n****************\n"
 
 tauxdereussitedetail()
+print "\n****************\n"
+globaltauxactions()
